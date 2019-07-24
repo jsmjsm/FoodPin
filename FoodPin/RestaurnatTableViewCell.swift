a286967d51e7cffbdfd3845e6421657987ec0203
LevyPan <levy-pan@outlook.com>
2019-07-23 23:38:42 -0700

    FoodPin - first push

---
+++ FoodPin/RestaurnatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurnatTableViewCell.swift
+//  FoodPin
+//
+//  Created by Levy Pan on 7/14/19.
+//  Copyright © 2019 LVP. All rights reserved.
+//
+
+import UIKit
+
+class RestaurnatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
